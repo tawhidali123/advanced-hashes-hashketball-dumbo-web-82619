8af9c677a50e50d3a9f019887be3ac82294f872d
Tawhid Ali <tawhidali126@gmail.com>
2019-08-20 03:14:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -295,19 +295,6 @@ def long_name_steals_a_ton?
   long_name == most_steals
 end
 
-# def long_name_steals_a_ton?
-#   holder = game_hash
-#   long_name = player_with_longest_name
-#   max_player = nil
-#   holder.each do |team, team_hash|
-#     team_hash[:players].each do |player_hash|
-#       max_player ||= player_hash
-#       max_player = player_hash if player_hash[:stats][:steals] > max_player[:stats][:steals]
-#     end
-#   end
-
-#   max_player[:name] == long_name
-# end
 
 
 
